d1c640e9fcc3d8fd7ec6426b8f1907036460a6e7
Richard Hladík <rihl@uralyx.cz>
2017-10-28 18:25:33 +0200

    The sc.sh script: remove run() (e() should be used instead), inline c()

--- src/sc.sh
+++ src/sc.sh
@@ -6,15 +6,9 @@ b(){
 	[ a"$1" = a-d ] && flags=-g
 	g++ -std=c++11 -lm -Wall -Wno-sign-compare -Wshadow $flags "$(name)".cpp -o "$(name)"
 }
-c(){
-	`[ a"$1" = a-d ] && echo valgrind` ./"$(name)"
-}
-run(){
-	b "$@" && c "$@"
-}
 e(){
 	b "$@"
-	for i in "$(name)".in*; do echo $i:; c "$@" < $i; done
+	for i in "$(name)".in*; do echo $i:; `[ a"$1" = a-d ] && echo valgrind` ./"$(name)" < $i; done
 }
 n(){
 	mkdir "$1"
