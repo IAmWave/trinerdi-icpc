6a99bb9352cb1d1edd62e59447a927fdae766e66
Richard Hladík <rihl@uralyx.cz>
2018-04-01 21:23:41 +0200

    sc.sh: address comments

--- src/trinerdi/sc.sh
+++ src/trinerdi/sc.sh
@@ -1,9 +1,9 @@
-alias t="basename \$(realpath .)" # Assumption: prints an alphanumeric string
 e(){ # run on [problem].in*
+	t=$(basename `pwd`)
 	if [ "$1" = -d ]; then fl=-g; v=valgrind
 	else fl=-O2\ -fsanitize=address; v=; fi
-	g++ -std=c++11 -lm -Wall -Wno-sign-compare -Wshadow $fl `t`.cpp -o `t`
-	for i in `t`.in*; do echo $i:; $v ./`t` < $i; done
+	g++ -std=c++11 -lm -Wall -Wno-sign-compare -Wshadow $fl $t.cpp -o $t
+	for i in $t.in*; do echo $i:; $v ./$t < $i; done
 }
 create(){
 	for i in {a..z}; do  # Change z as needed
