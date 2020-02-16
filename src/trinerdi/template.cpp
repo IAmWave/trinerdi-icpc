9fbe6e4acb1aeaace5f43d169baeca94a6613bed
Richard Hladík <rihl@uralyx.cz>
2020-02-16 21:48:38 +0530

    Add a template

---
+++ src/trinerdi/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define rep(i, a, n) for (int i = (a); i < (n); i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= (a); i--)
+
+int n;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin >> n;
+}
